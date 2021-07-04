22fde9e22f46a1d5fac6648367976540ab3a4645
costudiolabor <74557729+costudiolabor@users.noreply.github.com>
2021-07-04 23:44:46 +0300

    Create UI inventory

---
+++ Project/Assets/Scripts/Inventory.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Inventory : MonoBehaviour
+{
+    [SerializeField]
+    private Image[] Slots = new Image[5]; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
